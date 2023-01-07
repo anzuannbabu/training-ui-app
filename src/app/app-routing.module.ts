eb1f4c0ccb7e3da60740bca649cc9b4fbad3bbf4
Nassor Nassor <nassor.nassor@tra.go.tz>
2023-01-07 15:48:19 +0530

    initializing the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { DepartmentListComponent } from './pages/department-list/department-list.component';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
+import { NewDepartmentComponent } from './pages/new-department/new-department.component';
+import { NewEmployeeComponent } from './pages/new-employee/new-employee.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'employee-list',
+        component: EmployeeListComponent,
+      },
+      {
+        path: 'new-employee',
+        component: NewEmployeeComponent,
+      },
+      {
+        path: 'department-list',
+        component: DepartmentListComponent,
+      },
+      {
+        path: 'new-department',
+        component: NewDepartmentComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
+import { NewEmployeeComponent } from './pages/new-employee/new-employee.component';
+import { NewDepartmentComponent } from './pages/new-department/new-department.component';
+import { DepartmentListComponent } from './pages/department-list/department-list.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    LayoutComponent,
+    EmployeeListComponent,
+    NewEmployeeComponent,
+    NewDepartmentComponent,
+    DepartmentListComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
